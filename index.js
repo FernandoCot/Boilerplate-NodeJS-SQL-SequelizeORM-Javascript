13ff33bdbb879da9588fda2d4fbad1ee93ea8f6a
FernandoCot <fernandocalixtodev@gmail.com>
2019-12-24 16:19:42 -0300

    Handling unmatched endpoints error, and creating a generic global error treatment

--- index.js
+++ index.js
@@ -17,6 +17,22 @@ app.use(morgan('dev'));
 // Calling Routes
 app.use('/users', routeUsers);
 
+// Handling unmatched endpoints
+app.use((req, res, next) => {
+  const erro = new Error('Rota não encontrada');
+  erro.status = 404;
+  next(erro);
+})
+
+// Generic error treatment (You can pass status and message error)
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    erro: {
+      mensagem: error.message
+    }
+  })
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${3000}`));
