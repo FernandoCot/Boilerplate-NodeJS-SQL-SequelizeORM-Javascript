39e859d82b0df43721628ed930921543159f7bb1
Fernando Calixto <fernandocalixtodev@gmail.com>
2020-02-08 12:42:36 -0300

    Importing new validator lib (express-validator) and creating initial validations (sign_up)

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 // Core
 import express from 'express';
 const router = express.Router();
+import { check, validationResult } from 'express-validator';
 
 // Components
 import { User } from '../app/models';
@@ -35,22 +36,38 @@ router.get('/:id', verifyToken, async (req, res) => {
   res.json(singleUser);
 });
 
-router.post('/sign_up', async (req, res) => {
+router.post('/sign_up', [
+  check('name')
+    .notEmpty().withMessage("O campo 'name' é obrigatório!"),
+  check('email')
+    .notEmpty().withMessage("O campo 'email' é obrigatório!")
+    .isString().withMessage("O campo 'email' deve ser uma string!")
+    .isEmail().withMessage("Email inválido!"),
+  check('password')
+    .notEmpty().withMessage("O campo 'senha' é obrigatório!")
+    .isLength({ min: 8, max: 8 }).withMessage("A senha deve ter 8 dígitos!")
+    .isString().withMessage("O campo 'senha' deve ser uma string!")
+], async (req, res) => {
   let user;
-  try {
-    const hashedPassword = await encrypt(req.body.password);
-    user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword
-    });
-    res.json({
-      name: user.name,
-      email: user.email,
-      token: generateJWT(user),
-    });
-  } catch (err) {
-    res.status(422).json(err.errors)
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  } else {
+    try {
+      const hashedPassword = await encrypt(req.body.password);
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      });
+      res.json({
+        name: user.name,
+        email: user.email,
+        token: generateJWT(user),
+      });
+    } catch (err) {
+      res.status(422).json(err.errors)
+    }
   }
 });
 
