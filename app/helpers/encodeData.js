04147496266e2e3d21f014f446bd521732caa316
Fernando Calixto <fernandocalixtodev@gmail.com>
2020-02-08 00:50:10 -0300

    Removing the old hashing lib (crypto) and removing it's functions and consts. Adding new hashing lib bcrypt, and adding encrypt and decrypt functions. Minor improvements.

--- app/helpers/encodeData.js
+++ app/helpers/encodeData.js
@@ -1,41 +1,15 @@
-// import crypto from 'crypto';
 import bcrypt from 'bcrypt';
 
 const saltRounds = 10;
 
-const encrypt = (password) => {
-  bcrypt.hash(password, saltRounds, function (err, hash) {
-    console.log(hash)
-    return hash;
-  })
+const encrypt = async (password) => {
+  const hashedPass = await bcrypt.hash(password, saltRounds)
+  return hashedPass
 }
 
-const decrypt = (password) => {
-
+const decrypt = async (password, hash) => {
+  const unhashedPassword = await bcrypt.compare(password, hash)
+  return unhashedPassword
 };
-
-
-// iv = Initialization Vector
-
-// const key = crypto.randomBytes(32);
-// const iv = crypto.randomBytes(16);
-
-//const encrypt = (password) => {
-//  let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
-//  let encrypted = cipher.update(password);
-//  encrypted = Buffer.concat([encrypted, cipher.final()]);
-
-//  return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
-//};
-
-/*const decrypt = (password) => {
-  let iv = Buffer.from(password.iv, 'hex');
-  let encryptedPassword = Buffer.from(password.encryptedData, 'hex');
-  let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
-  let decrypted = decipher.update(encryptedPassword);
-  decrypted = Buffer.concat([decrypted, decipher.final()]);
-
-  return decrypted.toString();
-};*/
 
 export { encrypt, decrypt };
