fe5544c1138c462bbb446f294474c8e1b7314822
Fernando Calixto <fernandocalixtodev@gmail.com>
2020-02-09 00:22:57 -0300

    Adding some new attributes to the 'users' table

--- database/migrations/20191127022705-create-user.js
+++ database/migrations/20191127022705-create-user.js
@@ -3,18 +3,22 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Users', {
       id: {
+        primaryKey: true,
         allowNull: false,
         autoIncrement: true,
-        primaryKey: true,
         type: Sequelize.INTEGER
       },
       name: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       email: {
+        unique: true,
+        allowNull: false,
         type: Sequelize.STRING
       },
       password: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       createdAt: {
