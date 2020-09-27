7f9d08fdd12a71a34a591be26d188eb3bc5af1d3
Fernando Calixto <fernandocalixtodev@gmail.com>
2020-09-26 23:57:28 -0300

    fix: models import to match the new sequelize 6 requirements

--- app/models/index.js
+++ app/models/index.js
@@ -11,7 +11,7 @@ fs
   .readdirSync(__dirname)
   .filter(file => (file.indexOf('.') !== 0) && (file !== path.basename(__filename)) && (file.slice(-3) === '.js'))
   .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize);
     db[model.name] = model;
   });
 
