78b2b68b95a9167d430326a38743d94a32818ce5
Fernando Calixto <fernandocalixtodev@gmail.com>
2020-12-27 00:44:59 -0300

    chore: replacing a bad behavior lib (bcrypt) for "bcryptjs"

--- app/helpers/encodeData.js
+++ app/helpers/encodeData.js
@@ -1,14 +1,14 @@
-import bcrypt from 'bcrypt';
+import bcryptjs from 'bcryptjs';
 
 const saltRounds = 10;
 
 const encrypt = async (password) => {
-  const hashedPass = await bcrypt.hash(password, saltRounds)
+  const hashedPass = await bcryptjs.hash(password, saltRounds)
   return hashedPass
 }
 
 const decrypt = async (password, hash) => {
-  const unhashedPassword = await bcrypt.compare(password, hash)
+  const unhashedPassword = await bcryptjs.compare(password, hash)
   return unhashedPassword
 };
 
