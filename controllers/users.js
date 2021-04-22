4d241468c565283002de6d1b6c51dfd14ef6efeb
Fernando Calixto <fernandocalixtodev@gmail.com>
2021-04-22 20:26:18 -0300

    chore: changing the user controller to match the new linter pattern

--- controllers/users.js
+++ controllers/users.js
@@ -15,17 +15,17 @@ router.get('/', verifyToken, async (req, res) => {
   try {
     const users = await User.findAll({
       attributes: [
-        "id",
-        "name",
-        "email",
-        "createdAt",
-        "updatedAt",
+        'id',
+        'name',
+        'email',
+        'createdAt',
+        'updatedAt',
       ],
     });
-    res.status(200).json(users);
-  } catch(err) {
-    res.status(500).end();
-    console.log(err)
+    return res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).end();
   }
 });
 
@@ -35,32 +35,33 @@ router.get('/:id',
     param('id')
       .notEmpty().withMessage("O parâmetro 'id' é obrigatório!")
       .isInt().withMessage("O parâmetro 'id' deve ser um inteiro!"),
-  ], async (req, res) => {
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      } else {
-        try {
-          const singleUser = await User.findByPk((req.params.id), {
-            attributes: [
-              "id",
-              "name",
-              "email",
-              "createdAt",
-              "updatedAt",
-            ],
-          });
-          if (singleUser) {
-            res.status(200).json(singleUser);
-          } else {
-            res.status(404).json("Usuário inexistente!");
-          }
-        } catch(err) {
-          res.status(500).end();
-          console.log(err)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } else {
+      try {
+        const singleUser = await User.findByPk((req.params.id), {
+          attributes: [
+            'id',
+            'name',
+            'email',
+            'createdAt',
+            'updatedAt',
+          ],
+        });
+        if (singleUser) {
+          return res.status(200).json(singleUser);
+        } else {
+          return res.status(404).json('Usuário inexistente!');
         }
+      } catch (err) {
+        console.log(err);
+        return res.status(500).end();
       }
-});
+    }
+  });
 
 router.delete('/:id',
   [
@@ -68,24 +69,25 @@ router.delete('/:id',
     param('id')
       .notEmpty().withMessage("O parâmetro 'id' é obrigatório!")
       .isInt().withMessage("O parâmetro 'id' deve ser um inteiro!"),
-  ], async (req, res) => {
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      } else {
-        try {
-          const deleteSingleUser = await User.destroy({ where: { id: req.params.id } });
-          if (deleteSingleUser) {
-            res.status(200).json('Usuário deletado com sucesso!');
-          } else {
-            res.status(404).json("Usuário inexistente!");
-          }
-        } catch(err) {
-          res.status(500).end();
-          console.log(err)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } else {
+      try {
+        const deleteSingleUser = await User.destroy({ where: { id: req.params.id } });
+        if (deleteSingleUser) {
+          return res.status(200).json('Usuário deletado com sucesso!');
+        } else {
+          return res.status(404).json('Usuário inexistente!');
         }
+      } catch (err) {
+        console.log(err);
+        return res.status(500).end();
       }
-});
+    }
+  });
 
 router.patch('/:id',
   [
@@ -98,38 +100,38 @@ router.patch('/:id',
       .isString().withMessage("O campo 'nome' deve ser uma string!"),
     body('email')
       .notEmpty().withMessage("O campo 'email' é obrigatório!")
-      .isEmail().withMessage("Email inválido!"),
+      .isEmail().withMessage('Email inválido!'),
     body('password')
       .notEmpty().withMessage("O campo 'senha' é obrigatório!")
       .isString().withMessage("O campo 'senha' deve ser uma string!")
-      .isLength({ min: 8, max: 8 }).withMessage("A senha deve ter 8 dígitos!")
-  ], async (req, res) => {
+      .isLength({ min: 8, max: 8 }).withMessage('A senha deve ter 8 dígitos!'),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      } else {
-        try {
-          const hashedPassword = await encrypt(req.body.password);
-          const updateSingleUser = await User.update(
-            {
-              name: req.body.name,
-              email: req.body.email,
-              password: hashedPassword
-            },
-            { where: { id: req.params.id } }
-          );
-          if (Boolean(...updateSingleUser)) {
-            res.status(200).json('Usuário atualizado com sucesso!');
-          }
-          else {
-            res.status(404).json("Usuário inexistente!");
-          }
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } else {
+      try {
+        const hashedPassword = await encrypt(req.body.password);
+        const updateSingleUser = await User.update(
+          {
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+          },
+          { where: { id: req.params.id } },
+        );
+
+        if (updateSingleUser) {
+          return res.status(200).json('Usuário atualizado com sucesso!');
+        } else {
+          return res.status(404).json('Usuário inexistente!');
         }
-        catch (err) {
-          res.status(500).end();
-        }
+      } catch (err) {
+        return res.status(500).end();
       }
-});
+    }
+  });
 
 router.post('/sign_up',
   [
@@ -138,12 +140,13 @@ router.post('/sign_up',
       .isString().withMessage("O campo 'nome' deve ser uma string!"),
     body('email')
       .notEmpty().withMessage("O campo 'email' é obrigatório!")
-      .isEmail().withMessage("Email inválido!"),
+      .isEmail().withMessage('Email inválido!'),
     body('password')
       .notEmpty().withMessage("O campo 'senha' é obrigatório!")
       .isString().withMessage("O campo 'senha' deve ser uma string!")
-      .isLength({ min: 8, max: 8 }).withMessage("A senha deve ter 8 dígitos!")
-  ], async (req, res) => {
+      .isLength({ min: 8, max: 8 }).withMessage('A senha deve ter 8 dígitos!'),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -151,36 +154,37 @@ router.post('/sign_up',
       try {
         const existUser = await User.findOne({ where: { email: req.body.email } });
         if (existUser) {
-          res.status(409).json('Esse email já está sendo usado!');
+          return res.status(409).json('Esse email já está sendo usado!');
         } else {
           const hashedPassword = await encrypt(req.body.password);
           const user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: hashedPassword
+            password: hashedPassword,
           });
-          res.status(201).json({
+          return res.status(201).json({
             name: user.name,
             email: user.email,
             token: generateJWT(user),
           });
         }
       } catch (err) {
-        res.status(422).json(err.errors)
+        return res.status(422).json(err.errors);
       }
     }
-});
+  });
 
 router.post('/login',
   [
     body('email')
       .notEmpty().withMessage("O campo 'email' é obrigatório!")
-      .isEmail().withMessage("Email inválido!"),
+      .isEmail().withMessage('Email inválido!'),
     body('password')
       .notEmpty().withMessage("O campo 'senha' é obrigatório!")
       .isString().withMessage("O campo 'senha' deve ser uma string!")
-      .isLength({ min: 8, max: 8 }).withMessage("A senha deve ter 8 dígitos!")
-  ], async (req, res) => {
+      .isLength({ min: 8, max: 8 }).withMessage('A senha deve ter 8 dígitos!'),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -190,21 +194,21 @@ router.post('/login',
         if (user) {
           const decryptedPass = await decrypt(req.body.password, user.password);
           if (decryptedPass) {
-            res.status(200).json({
+            return res.status(200).json({
               name: user.name,
               email: user.email,
               token: generateJWT(user),
             });
           } else {
-            res.status(404).json('Informações incorretas!');
+            return res.status(404).json('Informações incorretas!');
           }
         } else {
-          res.status(404).json('Credenciais incorretas!');
+          return res.status(404).json('Credenciais incorretas!');
         }
       } catch (err) {
-        res.status(404).json(err.errors);
+        return res.status(404).json(err.errors);
       }
     }
-});
+  });
 
 export default router;
