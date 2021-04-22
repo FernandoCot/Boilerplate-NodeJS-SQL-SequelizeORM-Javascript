5522b4b2787490899f4d6b195e067642310d2588
Fernando Calixto <fernandocalixtodev@gmail.com>
2021-04-22 09:16:58 -0300

    chore: changing some files to match the new linter pattern

--- app/helpers/encodeData.js
+++ app/helpers/encodeData.js
@@ -3,13 +3,13 @@ import bcryptjs from 'bcryptjs';
 const saltRounds = 10;
 
 const encrypt = async (password) => {
-  const hashedPass = await bcryptjs.hash(password, saltRounds)
-  return hashedPass
-}
+  const hashedPass = await bcryptjs.hash(password, saltRounds);
+  return hashedPass;
+};
 
 const decrypt = async (password, hash) => {
-  const unhashedPassword = await bcryptjs.compare(password, hash)
-  return unhashedPassword
+  const unhashedPassword = await bcryptjs.compare(password, hash);
+  return unhashedPassword;
 };
 
 export { encrypt, decrypt };
--- app/helpers/jwt.js
+++ app/helpers/jwt.js
@@ -2,9 +2,9 @@ import jwt from 'jsonwebtoken';
 
 const SECRET = 'AH9SHASHUAUHDUHSUHSADHAHUSDHU';
 
-var signOptions = {
-  expiresIn: "12h",
-  algorithm: "HS256"
+const signOptions = {
+  expiresIn: '12h',
+  algorithm: 'HS256',
 };
 
 const generateJWT = (user) => {
@@ -18,19 +18,19 @@ const generateJWT = (user) => {
 
 // The func below verifies if the token exists and if it's still valid
 const verifyToken = (req, res, next) => {
-  const token = req.headers['authorization'];
+  const token = req.headers.authorization;
   if (!token) return res.status(400).json('Token não pode ser nulo!');
 
   jwt.verify(token, SECRET, (err) => {
     if (err) return res.status(401).json('Token expirado ou inválido!');
     next();
   });
-}
+};
 
 const decodeJWT = (token) => {
   const decoded = jwt.verify(token, SECRET);
   // console.log(decoded)
   return decoded;
-}
+};
 
 export { generateJWT, verifyToken, decodeJWT };
