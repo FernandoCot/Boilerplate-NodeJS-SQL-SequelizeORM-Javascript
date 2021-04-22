f9b2173263746f1aa616efb9c20d551fa9e0f2fc
Fernando Calixto <fernandocalixtodev@gmail.com>
2021-04-22 02:15:54 -0300

    chore: adding the try/catch pattern in the initial model test, and updating the test script to show the unfinished async requests

--- __tests__/integration/users.test.js
+++ __tests__/integration/users.test.js
@@ -3,14 +3,20 @@ const { encrypt } = require('../../app/helpers/encodeData');
 
 describe('users', () => {
   it('should create a new user', async (done) => {
-    const hashedPassword = await encrypt('12345678');
-    const randomString = Math.random().toString(36).substr(2, 9);
-    const createTest = await User.create({
-      name: 'Teste',
-      email: `${randomString}@gmail.com`,
-      password: hashedPassword
-    });
-    expect(createTest).toBeTruthy();
-    done();
+    let createTest = undefined;
+    try {
+      const hashedPassword = await encrypt('12345678');
+      const randomString = Math.random().toString(36).substr(2, 9);
+      createTest = await User.create({
+        name: 'Teste',
+        email: `${randomString}@gmail.com`,
+        password: hashedPassword
+      });
+      expect(createTest).toBeTruthy();
+      done();
+    } catch {
+      expect(createTest).toBeFalsy();
+      done();
+    }
   });
 });
