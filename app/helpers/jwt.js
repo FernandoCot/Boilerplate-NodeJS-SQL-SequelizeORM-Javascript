f05e021aca7559d16815788831ddc857cda6fc30
Fernando Calixto <fernandocalixtodev@gmail.com>
2021-04-24 01:03:01 -0300

    chore: doing some minor refactorings and changing the changing the eslint scanner folders

--- app/helpers/jwt.js
+++ app/helpers/jwt.js
@@ -13,18 +13,22 @@ const generateJWT = (user) => {
     name: user.name,
     email: user.email,
   };
+
   return jwt.sign(payload, SECRET, signOptions);
 };
 
 // The func below verifies if the token exists and if it's still valid
 const verifyToken = (req, res, next) => {
   const token = req.headers.authorization;
-  if (!token) return res.status(400).json('Token não pode ser nulo!');
 
-  jwt.verify(token, SECRET, (err) => {
-    if (err) return res.status(401).json('Token expirado ou inválido!');
-    next();
-  });
+  if (!token) {
+    return res.status(400).json('Token não pode ser nulo!');
+  } else {
+    return jwt.verify(token, SECRET, (err) => {
+      if (err) return res.status(401).json('Token expirado ou inválido!');
+      return next();
+    });
+  }
 };
 
 const decodeJWT = (token) => {
